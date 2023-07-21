5bab8629f5ffa0e50471c1414deacfa9f1fbee13
kristencai <kristen_cai@brown.edu>
2023-07-21 10:02:21 -0500

    remove anonymous feedback form

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -93,11 +93,11 @@ class HomePage extends React.Component {
 								Welcome!! CS17 (CSCI 0170) is an intro computer
 								science class at Brown University — emphasis on intro!!
 								We take your ideas very seriously and want to make a welcoming environment
-								for everyone who wants to learn CS — fill out our {" "}
+								for everyone who wants to learn CS! {/* — fill out our {" "}
 								<a href="https://docs.google.com/forms/d/e/1FAIpQLSc90q808orP2UEUYiZSWGvaqc2lLEe_-UdPoGmPa2Cj5vNSUg/viewform" target="_blank" rel="noopener noreferrer">
 									anonymous feedback form
 								</a>
-								{" "} to let us know if we can do anything better!
+		{" "} to let us know if we can do anything better!*/}
 							</p>
 							<p>
 								If you have any questions on content, post on Ed,
